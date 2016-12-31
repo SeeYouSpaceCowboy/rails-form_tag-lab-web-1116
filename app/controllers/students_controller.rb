5fbc48fb4be97e04a0d1180d643c139c3840960c
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2016-12-31 11:53:37 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,18 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
+  end
+
+  def new
   end
 
   def show
     @student = Student.find(params[:id])
   end
 
+  def create_new
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path(@student)
+  end
 end

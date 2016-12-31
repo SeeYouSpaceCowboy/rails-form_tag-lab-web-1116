3c0fac5cbf5646776ce5921158f6325ed570ba83
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2016-12-31 11:54:05 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,7 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def create_new
+  def insert
     @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to student_path(@student)
   end
